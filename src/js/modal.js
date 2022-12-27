fb46d8b8fcee2355d3ee41a6610f033ba080d5c5
Yuriy Pilipenko <yurik2061@gmail.com>
2022-12-27 23:36:26 +0200

    js

---
+++ src/js/modal.js
@@ -0,0 +1,22 @@
+$('.container-modal .title').each(function (idx, item) {
+    var winnerId = "winner-" + idx;
+     this.id = winnerId;
+     $(this).click(function(){
+       var btn = $("#winner-" + idx);
+       var span = $(".close");
+       var popId = $('#win-'+ idx);
+       btn.click(function() {
+          $(popId).addClass('on');
+          $('body').addClass('lorem');
+        }); 
+        span.click(function() {
+           $(popId).removeClass('on');
+           $('body').removeClass('lorem');
+         });
+       
+        
+     
+     });
+ });
+  
+ 
