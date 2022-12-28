d743b877482a5e1437f8a0d1f28ef02e6a402999
YuriyPilipenkoKH <113132127+YuriyPilipenkoKH@users.noreply.github.com>
2022-12-28 05:34:04 +0000

    Deploying to gh-pages from @ YuriyPilipenkoKH/ice4cream@9eca3de79a16beb96cd2fe1ce9b061ae4fdc1a5c 🚀

--- index.0a806476.js
+++
@@ -1,2 +0,0 @@
-$(".container-modal .title").each((function(n,i){var o="winner-"+n;this.id=o,$(this).click((function(){var i=$("#winner-"+n),o=$(".close"),c=$("#win-"+n);i.click((function(){$(c).addClass("on"),$("body").addClass("lorem")})),o.click((function(){$(c).removeClass("on"),$("body").removeClass("lorem")}))}))}));
-//# sourceMappingURL=index.0a806476.js.map
